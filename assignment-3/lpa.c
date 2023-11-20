0d01a89bbc1aa7d5038a1c97fc318fc638ee9225
Benjamin Chu <chu00071@umn.edu>
2023-11-20 08:25:31 -0600

    Implemented decomposition by edges

--- assignment-3/lpa.c
+++ assignment-3/lpa.c
@@ -4,6 +4,90 @@
 
 #include "utils.c"
 
+/**
+ * @brief (Unused) Splits the nodes of a graph into n ranges such
+ *        that each range has approximately the same unused of nodes
+ *
+ * @param graph The graph to split
+ * @param range_starts Where to write the starting indices of the ranges
+ * @param range_sizes Where to write the sizes of the ranges
+ * @param n The number of ranges to create
+ */
+void split_by_nodes(graph_t *graph, int *range_starts, int *range_sizes, int n)
+{
+    int next = 0;
+    for (int i = 0; i < size; i++) {
+        range_starts[i] = next;
+        next = graph.num_nodes * (i+1) / size;
+        range_sizes[i] = next - range_starts[i];
+    }
+}
+
+/**
+ * @brief Splits the nodes of a graph into n ranges by their neighbor counts
+ *        Rather than splitting so that each range has the same amount of nodes,
+ *        This splits the graph so that the total number of neighbors of its nodes
+ *        is approximately the same
+ *
+ * @param graph The graph to split
+ * @param range_starts Where to write the starting indices of the ranges
+ * @param range_sizes Where to write the sizes of the ranges
+ * @param n The number of ranges to create
+ */
+void split_by_neighbors(graph_t *graph, int *range_starts, int *range_sizes, int n)
+{
+    // First, the target neighbor count per range is computed
+    // as twice the number of edges divided by the number of ranges
+    // Then, iterate over the subarray while adding the neighber counts up
+    // When the sum is close enough to the target, create a new subarray
+
+    double target = 2 * graph->num_edges / n;
+    double cur_sum = 0;
+    double cur_diff = target;
+
+    int prev_start = 0;
+    int cur_range = 0;
+    for (int i = 0; i < graph->num_nodes; i++) {
+        // Compute new sum and the difference between it and the target
+        double next_sum = cur_sum + graph->counts[i];
+        double next_diff = next_sum - target;
+        if (next_diff < 0)
+            next_diff = -next_diff;
+
+        // If adding another element would move away from the target,
+        // create the next subarray
+        if (next_diff > cur_diff) {
+            // Set the values for this subarray
+            range_starts[cur_range] = prev_start;
+            range_sizes[cur_range] = i - prev_start;
+            prev_start = i;
+            cur_range++;
+
+            // Recompute sum and diff
+            next_sum -= target;
+            next_diff -= target;
+            if (next_diff < 0)
+                next_diff = -next_diff;
+
+            // Break early at last range
+            if (cur_range+1 == n)
+                break;
+        }
+
+        // Set sum and diff for next iteration
+        cur_sum = next_sum;
+        cur_diff = next_diff;
+    }
+
+    // Fill in remaining ranges. Usually only one range is left.
+    // However, there can be more ranges if n is greater than the node count.
+    for (; cur_range < n; cur_range++) {
+        range_starts[cur_range] = prev_start;
+        range_sizes[cur_range] = graph->num_nodes - prev_start;
+        prev_start = graph->num_nodes;
+    }
+}
+
 int main(int argc, char** argv)
 {
     int rank, size;
@@ -37,13 +121,15 @@ int main(int argc, char** argv)
     int *range_starts = (int*) malloc(size * sizeof(int));
     int *range_sizes = (int*) malloc(size * sizeof(int));
 
-    // Determine ranges that each thread will work on
-    int next = 0;
-    for (int i = 0; i < size; i++) {
-        range_starts[i] = next;
-        next = graph.num_nodes * (i+1) / size;
-        range_sizes[i] = next - range_starts[i];
+    // First process determines what range each process will work on
+    if (rank == 0) {
+        split_by_neighbors(&graph, range_starts, range_sizes, size);
+        // split_by_nodes(&graph, range_starts, range_sizes, size);
     }
+
+    // Ranges are broadcasted to other processes
+    MPI_Bcast(range_starts, size, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(range_sizes, size, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Get this thread's range
     int range_start = range_starts[rank];
@@ -103,10 +189,8 @@ int main(int argc, char** argv)
     }
 
     // After graph data is scattered,
-    // neighbors[offsets[i-range_start]+j]
-    // is the jth neighbor of the node i and
-    // counts[i-range_start] is the number of
-    // neighbors of node i
+    // counts[i-range_start] is the number of neighbors of node i
+    // neighbors[offsets[i-range_start]+j] is the jth neighbor of the node i
 
     // STEPS 2-5 ---------------------------------------------------------------
     MPI_Barrier(MPI_COMM_WORLD);
